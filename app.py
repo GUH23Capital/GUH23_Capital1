962b6c73b200ae67aaa60fe639c679e7757e4cd3
Ente Wang <ente.wang@student.manchester.ac.uk>
2023-10-29 10:08:35 +0000

    web funtion done

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 import folium
 import pandas as pd
 from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Load data from the CSV file
 def load_data():
     return pd.read_csv("transactions_with_details.csv")
 
-def filter_data(name, m):
-    df = load_data()
-    # Create a map centered on the city of York
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    location = geolocator.geocode(f"{name}, UK")
-    if location:
-        map_center = [location.latitude, location.longitude]
+@app.route("/sfaf", methods=['GET','POST'])
+def filter_data():
+    if request.method == 'POST':
+        name = request.form['input']
+        print(name)
+        if isinstance(name,str):
+            df = load_data()
+            # Create a map centered on the city of York
+            geolocator = Nominatim(user_agent="geoapiExercises")
+            location = geolocator.geocode(f"{name}, UK")
+            if location:
+                map_center = [location.latitude, location.longitude]
+            else:
+                # Fallback coordinates if geocoding fails
+                map_center = [53.9590, -1.0815]
+
+            m = folium.Map(location=[map_center[0], map_center[1]], zoom_start=10)
+
+
+            # Add markers for points in the city
+            for index, row in df.iterrows():
+                if isinstance(row['City'], str) and isinstance(name, str) and (str(row['County']).lower()  == name.lower() or str(row['City']).lower()  == name.lower() or str(row['Road']).lower()  == name.lower()):
+                    popup_text = f"<b>{row['Merchant Name']}</b><br>Timestamp: {row['Timestamp']}<br>Road:{row['Road']}<br>City: {row['City']}<br>County: {row['County']}"
+                    folium.Marker(
+                        location=[row['Latitude'], row['Longitude']],
+                        popup=folium.Popup(popup_text, max_width=200),
+                    ).add_to(m)
+
+            # Save the map to an HTML file (optional)
+            m.save("./templates/searchedMap.html")
+            flash('Biu')
+            return render_template('searchedMap.html')
+        else:
+            flash('Not a valid input')
+            return render_template('staf2.html')
     else:
-        # Fallback coordinates if geocoding fails
-        map_center = [53.9590, -1.0815]
-
-    m = folium.Map(location=[map_center[0], map_center[1]], zoom_start=10)
-
-
-    # Add markers for points in the city
-    for index, row in df.iterrows():
-        if isinstance(row['City'], str) and row['City'].lower() == name.lower():
-            popup_text = f"<b>{row['Merchant Name']}</b><br>Timestamp: {row['Timestamp']}<br>Road:{row['Road']}<br>City: {row['City']}<br>County: {row['County']}"
-            folium.Marker(
-                location=[row['Latitude'], row['Longitude']],
-                popup=folium.Popup(popup_text, max_width=200),
-            ).add_to(m)
-
-    # Save the map to an HTML file (optional)
-    m.save("searchedMap.html")
-
-
+        return render_template('sfaf.html')
 
 # Your Flask routes and logic go here
 @app.route('/')
@@ -61,7 +72,7 @@ def map():
         ).add_to(m)
 
     # Save the map to an HTML file (optional)
-    m.save("map.html")
+    m.save("./templates/map.html")
     return render_template('map.html')
 
 if __name__ == '__main__':
